05ebb9370e9326f32647bc7147c7af133f12e019
25397520 <amontenegro025@gmail.com>
2023-04-02 13:34:51 -0500

    Creacion de repositorio

---
+++ src/main/java/com/nisum/userws/UserwsApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.userws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/userws/UserwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.userws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
